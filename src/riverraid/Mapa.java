09bb2ab9e3612ef5fac05494591f99afd299af64
Alejandro <espinel_11@hotmail.com>
2019-04-27 10:45:04 -0500

    modificando

--- src/riverraid/Mapa.java
+++ src/riverraid/Mapa.java
@@ -18,32 +18,16 @@ public class Mapa  extends JFrame{
  this.setVisible(true);
  this.setResizable(false);
  this.setLayout(null);
- eventos();
- movimiento();
+
  
  }   
-public void movimiento(){
-Rectangle nave= new Rectangle(100,100,20,30);
-ImageIcon icon=new ImageIcon(nave);
 
-} 
- 
- 
-public void  eventos(){
-    addWindowListener(new WindowAdapter(){
-    public void windowClosing(WindowEvent e){
-    System.exit(0);
-    }
-    
-    
-    });
+
+
+
 
 }
 
-    private void add(Rectangle nave) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
     
     
-    
-}
+
